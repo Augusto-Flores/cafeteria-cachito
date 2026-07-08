764ab68917bd68dca795847e4c1773302073f0dd
Augusto <floresaugusto2000@gmail.com>
2026-07-07 20:09:00 -0500

    port por si solicita
    Added commented lines for optional database port configuration.

--- config/config.php
+++ config/config.php
@@ -11,12 +11,14 @@ function getPDO(): PDO
 
     // Configuración por defecto de XAMPP
     $host = "localhost";
+    // $port = "3307"; // <-- Por si solicita un puerto ingresar el puerto de su BD
     $db   = 'cafeteria_db';
     $user = 'root';
     $pass = ''; // XAMPP por defecto NO tiene contraseña. Se deja en blanco.
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host={$host};dbname={$db};charset={$charset}";
+   // $dsn = "mysql:host={$host};port={$port};dbname={$db};charset={$charset}";// <-- AQUÍ SE AÑADIÓ port={$port}; A LA CADENA DE CONEXIÓN
 
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
